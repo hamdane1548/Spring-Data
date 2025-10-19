c528a90f1f6c3d51bec0b04a3a772b8cd1696348
hamdane1548 <oussamahamdane80@gmail.com>
2025-10-19 12:33:15 +0100

    final version

--- src/main/java/ma/mundia/tp2/Tp2Application.java
+++ src/main/java/ma/mundia/tp2/Tp2Application.java
@@ -1,10 +1,52 @@
 package ma.mundia.tp2;
 
+import ma.mundia.tp2.entites.Product;
+import ma.mundia.tp2.repostory.InterfaceReposotory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class Tp2Application {
+public class Tp2Application implements CommandLineRunner {
+    @Autowired
+    private InterfaceReposotory productReposotory;
+    @Override
+    public void run(String... args) throws Exception {
+      //  productReposotory.save(new Product(null,"Computer",9255,5));
+      //  productReposotory.save(new Product(null,"Printer",1855,9));
+      //  productReposotory.save(new Product(null,"SmartPhone",5255,15));
+        List<Product> productList = productReposotory.findAll();
+        productList.forEach(produit->{
+            System.out.println(produit.toString());
+        });
+        try{
+            Product product=productReposotory.findById(Long.valueOf(1)).orElseThrow(()->new RuntimeException("produit no trouve"));
+            System.out.println("************");
+            System.out.println(product.getId());
+            System.out.println(product.getNom());
+            System.out.println(product.getPrix());
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+       List<Product> produi= productReposotory.findByNomContains("n");
+        produi.forEach(produit->{
+            System.out.println(produit.toString());
+        });
+        System.out.println("list produit 2");
+        List<Product> produi2= productReposotory.search("%C%");
+        produi2.forEach(produitr->{
+            System.out.println(produitr.toString());
+        });
+        System.out.println("-----------price------------");
+        List<Product> price= productReposotory.findByPrixGreaterThan(555);
+        price.forEach(pricepri->{
+            System.out.println(pricepri.toString());
+        });
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Tp2Application.class, args);
--- src/main/java/ma/mundia/tp2/entites/Product.java
+++ src/main/java/ma/mundia/tp2/entites/Product.java
@@ -1,8 +1,19 @@
 package ma.mundia.tp2.entites;
-@Data
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
     private double prix;
     private int quantity;
+
 }
